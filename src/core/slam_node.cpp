69a9ac5ec2ad54f8cec950850bf92fa5f54fc161
binxni <126181703+binxni@users.noreply.github.com>
2025-08-11 20:50:02 +0900

    Include tf2 utils header for getYaw

--- src/core/slam_node.cpp
+++ src/core/slam_node.cpp
@@ -2,6 +2,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2/utils.h"
 
 #include <cmath>
 #include <memory>
